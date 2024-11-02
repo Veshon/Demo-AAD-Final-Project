77420d4170415b2198217196d9eaabe6b10529df
veshon <vashonmabima12@gmail.com>
2024-11-02 19:13:33 +0530

    Modifications

--- src/main/java/com/example/demo/dao/FieldsDAO.java
+++
@@ -1,9 +0,0 @@
-/*
-package com.example.demo.dao;
-
-import com.example.demo.entity.impl.FieldEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FieldsDAO extends JpaRepository<FieldEntity, String> {
-}
-*/
