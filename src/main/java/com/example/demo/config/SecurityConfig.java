a947929ca329179a0c3b7f750e5f79f38ed0a693
veshon <vashonmabima12@gmail.com>
2024-11-04 14:53:05 +0530

    Update Log Completed

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -40,4 +41,39 @@ public class SecurityConfig {
         return new InMemoryUserDetailsManager(principleUser);
     }
 }
+
+*/
+/*@EnableWebSecurity
+public class SecurityConfig {
+
+    @Value("${secure.username}")
+    private String userName;
+    @Value("${secure.password}")
+    private String password;
+    @Value("${secure.role}")
+    private String role;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.cors() // Enable CORS configuration
+                .and()
+                .csrf().disable() // Disable CSRF if necessary
+                .authorizeRequests()
+                .anyRequest().permitAll(); // Adjust authorization as needed
+
+        return http.build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+        UserDetails principleUser = User.withDefaultPasswordEncoder()
+                .username(userName)
+                .password(password)
+                .roles(role).build();
+        return new InMemoryUserDetailsManager(principleUser);
+    }
+}*//*
+
+
+
 */
