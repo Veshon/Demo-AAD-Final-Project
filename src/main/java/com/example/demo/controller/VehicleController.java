018d54dc70136ba211b6d0df6597c4f9f147b114
veshon <vashonmabima12@gmail.com>
2024-11-11 12:41:48 +0530

    Add logs to vehicle

--- src/main/java/com/example/demo/controller/VehicleController.java
+++ src/main/java/com/example/demo/controller/VehicleController.java
@@ -8,6 +8,8 @@ import com.example.demo.dto.impl.VehicleDTO;
 import com.example.demo.exception.DataPersistException;
 import com.example.demo.exception.FieldNotFoundException;
 import com.example.demo.service.VehicleService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -21,6 +23,7 @@ import java.util.regex.Pattern;
 @RequestMapping("api/v1/vehicles")
 
 public class VehicleController {
+    static Logger logger = LoggerFactory.getLogger(VehicleController.class);
 
     @Autowired
     private VehicleService vehicleService;
@@ -30,18 +33,22 @@ public class VehicleController {
     public ResponseEntity<Void> saveVehicle(@RequestBody VehicleDTO vehicleDTO) {
         try {
             vehicleService.saveVehicle(vehicleDTO);
+            logger.info("Vehicle POST method executed.");
             return new ResponseEntity<>(HttpStatus.CREATED);
         }catch (DataPersistException e){
             e.printStackTrace();
+            logger.info("Vehicle POST method not executed.");
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }catch (Exception e){
             e.printStackTrace();
+            logger.info("Vehicle POST method not executed.");
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public List<VehicleDTO> getAllVehicles(){
+        logger.info("Vehicle GTE_ALL method executed.");
         return vehicleService.getAllVehicles();
     }
 
@@ -51,8 +58,10 @@ public class VehicleController {
         Pattern regexPattern = Pattern.compile(regexForUserID);
         var regexMatcher = regexPattern.matcher(code);
         if (!regexMatcher.matches()) {
+            logger.info("Vehicle GTE method not executed.");
             return (VehicleStatus) new SelectedErrorStatus(1,"Vehicle code is not valid");
         }
+        logger.info("Vehicle GTE method executed.");
         return vehicleService.getVehicle(code);
     }
 
@@ -66,17 +75,21 @@ public class VehicleController {
         try {
 
             if (!regexMatcher.matches()){
+                logger.info("Vehicle DELETE method not executed.");
                 return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
             }
 
             vehicleService.deleteVehicle(code);
+            logger.info("Vehicle DELETE method executed.");
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 
         }catch (FieldNotFoundException e){
             e.printStackTrace();
+            logger.info("Vehicle DELETE method not executed.");
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }catch (Exception e){
             e.printStackTrace();
+            logger.info("Vehicle DELETE method not executed.");
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 
         }
@@ -90,15 +103,19 @@ public class VehicleController {
         var regexMatcher = regexPattern.matcher(code);
         try {
             if(!regexMatcher.matches() || updatedVehicleDTO == null){
+                logger.info("Vehicle UPDATE method not executed.");
                 return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
             }
             vehicleService.updateVehicle(code,updatedVehicleDTO);
+            logger.info("Vehicle UPDATE method executed.");
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         }catch (FieldNotFoundException e){
             e.printStackTrace();
+            logger.info("Vehicle UPDATE method not executed.");
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }catch (Exception e){
             e.printStackTrace();
+            logger.info("Vehicle UPDATE method not executed.");
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
