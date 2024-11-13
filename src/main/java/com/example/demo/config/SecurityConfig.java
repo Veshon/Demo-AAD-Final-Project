620bb19e41c4dfe547ab017abaa81bfcf84dd1ee
veshon <vashonmabima12@gmail.com>
2024-11-13 11:27:10 +0530

    Restricted all CRUD Operations in crops form to manager

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -41,6 +41,12 @@ public class SecurityConfig {
                                 .requestMatchers(HttpMethod.GET, "/api/v1/fields/**").hasAuthority("ROLE_MANAGER") // Only managers can perform GET
                                 .requestMatchers(HttpMethod.PUT, "/api/v1/fields/**").hasAuthority("ROLE_MANAGER") // Only managers can perform PUT
                                 .requestMatchers(HttpMethod.DELETE, "/api/v1/fields/**").hasAuthority("ROLE_MANAGER") // Only managers can perform DELETE
+
+                                .requestMatchers(HttpMethod.POST, "/api/v1/crops/**").hasAuthority("ROLE_MANAGER") // Only managers can perform POST
+                                .requestMatchers(HttpMethod.GET, "/api/v1/crops/**").hasAuthority("ROLE_MANAGER") // Only managers can perform GET
+                                .requestMatchers(HttpMethod.PUT, "/api/v1/crops/**").hasAuthority("ROLE_MANAGER") // Only managers can perform PUT
+                                .requestMatchers(HttpMethod.DELETE, "/api/v1/crops/**").hasAuthority("ROLE_MANAGER") // Only managers can perform DELETE
+
 /*                                .requestMatchers(HttpMethod.POST, "/api/v1/fields/**").hasAuthority("ROLE_ADMIN") // Only admins can perform POST
                                 .requestMatchers("/api/v1/fields/**")
                                 .hasRole("ADMIN")
